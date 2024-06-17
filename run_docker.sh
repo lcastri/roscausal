06cb797495202e2b39c4ed09a3e997508f4d5185
Luca Castri <lucacastri94@gmail.com>
2024-06-17 21:27:26 +0100

    docker ready

--- run_docker.sh
+++ run_docker.sh
@@ -8,21 +8,6 @@ container_folder=/root/shared
 xhost +local:
 
 echo " "
-echo "Starting ${container_name} engine..."
-systemctl start docker
-
-echo " "
-echo "Building ${container_name} docker..."
-docker build -f docker/Dockerfile -t ${image_name} .
-
-echo " "
-echo "Removing old images..."
-dangling_images=$(docker images -qa -f 'dangling=true')
-if [ -n "$dangling_images" ]; then
-    docker rmi $dangling_images
-fi
-
-echo " "
 echo "Running ${container_name} container..."
 docker run --name ${container_name} -it --rm  -e DISPLAY=$DISPLAY -e NVIDIA_DRIVER_CAPABILITIES=all -e QT_X11_NO_MITSHM=1 -v $HOME/.Xauthority:/root/.Xauthority:rw -v /tmp/.X11-unix:/tmp/.X11-unix -v $host_folder:$container_folder --privileged --gpus all --network host ${image_name}
 
