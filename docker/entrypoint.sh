083595ca03908dca5a7b7d2ac847a746428a348a
Luca Castri <lucacastri94@gmail.com>
2024-06-18 13:11:58 +0100

    tmule file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,12 +15,16 @@ echo " "
 
   echo "Container is now running."
   echo " "
-   source /opt/ros/noetic/setup.bash
-   cd ~/ros_ws
-   catkin build
-   source ~/ros_ws/devel/setup.bash
-   echo "source ~/ros_ws/devel/setup.bash" >> ~/.bashrc
-   exec "/bin/bash"
+  echo "function rc-start(){  tmule -c ~/ros_ws/src/ROSCausal/roscausal_tmule/tmule/roscausal.yaml -W 3 launch ; }" >> ~/.bashrc
+  echo "function rc-stop(){  tmule -c ~/ros_ws/src/ROSCausal/roscausal_tmule/tmule/roscausal.yaml terminate ; }" >> ~/.bashrc
+  echo "function rc-show(){  tmux a -t roscausal ; }" >> ~/.bashrc
+
+  source /opt/ros/noetic/setup.bash
+  cd ~/ros_ws
+  catkin build
+  source ~/ros_ws/devel/setup.bash
+  echo "source ~/ros_ws/devel/setup.bash" >> ~/.bashrc
+  exec "/bin/bash"
 
 } || {
 
